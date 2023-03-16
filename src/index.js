d2b930ecf69e71377653b2a9fda6bf1bee1a6113
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-16 09:48:37 -0500

    78. Redux Review and Setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+// import { createStore, } from "redux"; // Leaving this as a reminder that this is deprecated
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
