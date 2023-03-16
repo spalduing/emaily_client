ed09557ef76c591707b8776d0ce6a2be4ed58602
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-16 11:07:53 -0500

    81.-85. Always Visible Components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboad</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <>
+          <Header />
+          <Route exact={true} path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
