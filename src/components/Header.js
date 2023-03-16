d2bda130b3578fcdb8ab0cb782658db1d72bac71
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-16 11:42:10 -0500

    86.-88. Header Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,18 @@
-const Header = () => <h2>Header</h2>;
+const Header = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="left brand-logo">
+          Emaily
+        </a>
+        <ul id="nav-mobile" class="right ">
+          <li>
+            <a href="sass.html">Login With Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
