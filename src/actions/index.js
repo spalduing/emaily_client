5388326e1490566ca3ab9ad9066dfb3af58287b9
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-17 07:57:31 -0500

    94. Refatoring to Async/Await

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,23 @@ import axios from "axios";
 
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return function (dispatch) {
-    axios
-      .get("/api/current_user")
-      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
-  };
+// // Common/intuitive way of implementing this function/
+// export const fetchUser = () => {
+//   return function (dispatch) {
+//     axios
+//       .get("/api/current_user")
+//       .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
+
+// // Reduced version of the above implementation.
+// export const fetchUser = () => async (dispatch) => {
+//   const res = await axios.get("/api/current_user");
+
+//   dispatch({ type: FETCH_USER, payload: res });
+// };
+
+// A even more reduced version of the above implementation.
+export const fetchUser = () => async (dispatch) => {
+  dispatch({ type: FETCH_USER, payload: await axios.get("/api/current_user") });
 };
