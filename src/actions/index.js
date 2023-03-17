a9cb75c00a45dc469a9d47ac224c3ef65dcff0fb
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-03-17 10:39:05 -0500

    95.-101. Link Tags.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,18 +7,18 @@ import { FETCH_USER } from "./types";
 //   return function (dispatch) {
 //     axios
 //       .get("/api/current_user")
-//       .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+//       .then((res) => dispatch({ type: FETCH_USER, payload: res.data }));
 //   };
 // };
 
-// // Reduced version of the above implementation.
-// export const fetchUser = () => async (dispatch) => {
-//   const res = await axios.get("/api/current_user");
-
-//   dispatch({ type: FETCH_USER, payload: res });
-// };
-
-// A even more reduced version of the above implementation.
+// Reduced version of the above implementation.
 export const fetchUser = () => async (dispatch) => {
-  dispatch({ type: FETCH_USER, payload: await axios.get("/api/current_user") });
+  const res = await axios.get("/api/current_user");
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// // A even more reduced version of the above implementation.
+// export const fetchUser = () => async (dispatch) => {
+//   dispatch({ type: FETCH_USER, payload: await axios.get("/api/current_user").data });
+// };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,48 @@
-const Header = () => {
-  return (
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="left brand-logo">
-          Emaily
-        </a>
-        <ul id="nav-mobile" class="right ">
-          <li>
-            <a href="sass.html">Login With Google</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-export default Header;
+import * as actions from "../actions";
+
+class Header extends Component {
+  renderContent() {
+    const auth = this.props.auth;
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Log Out</a>
+          </li>
+        );
+    }
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <Link to={this.props.auth ? "/surveys" : "/"} class="left brand-logo">
+            Emaily
+          </Link>
+          <ul id="nav-mobile" class="right ">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
